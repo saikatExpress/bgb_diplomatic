cc5a47dc7c6066cd37e130941fab8062a15edcf6
Saikat Talukder <saikathosen444@gmail.com>
2025-06-03 17:47:41 +0600

    master layout setup done

---
+++ resources/views/super/layout/style.blade.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="{{ asset('assets/super/vendors/mdi/css/materialdesignicons.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/super/vendors/base/vendor.bundle.base.css') }}">
+
+<link rel="stylesheet" href="{{ asset('assets/super/css/style.css') }}">
