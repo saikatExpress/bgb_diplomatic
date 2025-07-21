5dc7573f949725636aeb4fc8e06793b5032968b7
Saikat Talukder <saikathosen444@gmail.com>
2025-07-21 21:12:26 +0600

    clear controller

--- routes/developer.php
+++ routes/developer.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Dev\DatabaseController;
 use App\Http\Controllers\Dev\DeveloperController;
 use App\Http\Controllers\Dev\TableController;
 use Illuminate\Support\Facades\Route;
